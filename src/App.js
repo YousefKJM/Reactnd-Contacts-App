6c781c955fa10afdbca6542c9b3ecfcf67a2deb6
Yousef Majeed <uaso0of056@gmail.com>
2020-04-28 08:07:21 +0300

    Pass Data Into Components With Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ListContacts from "./ListContacts";
+
+const contacts = [
+	{
+		id: "karen",
+		name: "Karen Isgrigg",
+		handle: "@karen_isgrigg",
+		avatarURL: "http://localhost:5001/michael.jpg",
+	},
+	{
+		id: "richard",
+		name: "Richard Kalehoff",
+		handle: "@richardkalehoff",
+		avatarURL: "http://localhost:5001/ryan.jpg",
+	},
+	{
+		id: "tyler",
+		name: "Tyler McGinnis",
+		handle: "@tylermcginnis",
+		avatarURL: "http://localhost:5001/tyler.jpg",
+	},
+];
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<ListContacts contacts={contacts} />
+			</div>
+		);
+	}
 }
 
 export default App;
